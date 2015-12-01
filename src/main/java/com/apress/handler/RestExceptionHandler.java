d906e61c16e60b9ac3177cc4581e3e28b663ece2
YAWEI LI <jsyclyw@gmail.com>
2015-12-01 15:32:29 -0500

    add more error handler

--- src/main/java/com/apress/handler/RestExceptionHandler.java
+++ src/main/java/com/apress/handler/RestExceptionHandler.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.apress.dto.error.ErrorDetail;
 import com.apress.dto.error.ValidationError;
@@ -33,7 +37,7 @@ public class RestExceptionHandler {
 		return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
 	}*/
 	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
+	/*@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<?> handleValidationError(
 			MethodArgumentNotValidException manve, HttpServletRequest request){
 		ErrorDetail errorDetail = new ErrorDetail();
@@ -62,6 +66,41 @@ public class RestExceptionHandler {
 			validationErrorList.add(validationError);
 		}
 		return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
+	}*/
+	
+	@Inject
+	private MessageSource messageSource;
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public @ResponseBody ErrorDetail handleValidationError(
+			MethodArgumentNotValidException manve, HttpServletRequest request){
+		ErrorDetail errorDetail = new ErrorDetail();
+		//Populate errorDetail instance
+		errorDetail.setTimeStamp(new Date().getTime());
+		errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+		String requestPath = (String)request.getAttribute("javax.servlet.error.request_uri");
+		if(requestPath == null){
+			requestPath = request.getRequestURI();
+		}
+		errorDetail.setTitle("Validation Failed");
+		errorDetail.setDetail("Input validation failed");
+		errorDetail.setDeveloperMessage(manve.getClass().getName());
+		
+		//Create ValidationError instances
+		List<FieldError> fieldErrors = manve.getBindingResult().getFieldErrors();
+		for(FieldError fe : fieldErrors){
+			List<ValidationError> validationErrorList = errorDetail.getErrors().get(fe.getField());
+			if(validationErrorList == null){
+				validationErrorList = new ArrayList<ValidationError>();
+				errorDetail.getErrors().put(fe.getField(), validationErrorList);
+			}
+			ValidationError validationError = new ValidationError();
+			validationError.setCode(fe.getCode());
+			validationError.setMessage(messageSource.getMessage(fe, null));
+			validationErrorList.add(validationError);
+		}
+		return errorDetail;
 	}
 	
 }
