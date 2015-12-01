bc8343e1d9bb89543c414657978de7fdf3f901b7
YAWEI LI <jsyclyw@gmail.com>
2015-12-01 15:54:14 -0500

    add swagger for documenting REST Services

--- src/main/java/com/apress/QuickPollApplication.java
+++ src/main/java/com/apress/QuickPollApplication.java
@@ -3,10 +3,14 @@ package com.apress;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
+
 @SpringBootApplication
+@EnableSwagger
 public class QuickPollApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuickPollApplication.class, args);
     }
+    
 }
