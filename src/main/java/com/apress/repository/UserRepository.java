2e9489ac381f713c056bf8983c5e89fb3fe7ce18
YAWEI LI <jsyclyw@gmail.com>
2015-12-14 13:26:51 -0500

    save changes

---
+++ src/main/java/com/apress/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.apress.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.apress.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+
+	public User findByUsername(String name);
+	
+}
